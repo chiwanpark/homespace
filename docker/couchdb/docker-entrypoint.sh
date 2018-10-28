98c58d7b46b2dbe66c35ffc07ccc99f8e16e46e2
Chiwan Park <chiwanpark@icloud.com>
2018-10-28 22:46:00 +0900

    Update entrypoint script for CouchDB

--- docker/couchdb/docker-entrypoint.sh
+++ docker/couchdb/docker-entrypoint.sh
@@ -12,15 +12,13 @@ if [ "$1" = "couchdb" ]; then
     set -- /opt/couchdb/bin/couchdb "$@"
 fi
 
-if [ ! -s "/data/couchdb/_users.couch" ]; then
-    if [ ! -z "$NODENAME" ]; then
-        NODE_IP=$(cat /run/secrets/clusters | grep $NODENAME | awk '{split($0,a,"|"); print a[2]}')
-        echo "\n-name couchdb@${NODE_IP}" >> /opt/couchdb/etc/vm.args
-    fi
-
-    COUCHDB_ADMIN_USER=$(cat /run/secrets/couchdb-auth | awk '{split($0,a,"|"); print a[1]}')
-    COUCHDB_ADMIN_PASS=$(cat /run/secrets/couchdb-auth | awk '{split($0,a,"|"); print a[2]}')
-    echo "\n[admins]\n${COUCHDB_ADMIN_USER} = ${COUCHDB_ADMIN_PASS}" >> /opt/couchdb/etc/local.d/admin.ini
+if [ ! -z "$NODENAME" ]; then
+    NODE_IP=$(cat /run/secrets/clusters | grep $NODENAME | awk '{split($0,a,"|"); print a[2]}')
+    echo "\n-name couchdb@${NODE_IP}" >> /opt/couchdb/etc/vm.args
 fi
+
+COUCHDB_ADMIN_USER=$(cat /run/secrets/couchdb-auth | awk '{split($0,a,"|"); print a[1]}')
+COUCHDB_ADMIN_PASS=$(cat /run/secrets/couchdb-auth | awk '{split($0,a,"|"); print a[2]}')
+echo "\n[admins]\n${COUCHDB_ADMIN_USER} = ${COUCHDB_ADMIN_PASS}" >> /opt/couchdb/etc/local.d/admin.ini
 
 exec "$@"
